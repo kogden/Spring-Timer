4b2064805c95ebc8e2a97d2e73386500f5d137c5
Kevin Ogden <kogden2@albany.edu>
2017-07-24 18:41:44 -0400

    First commit

---
+++ src/main/java/timer/App.java
@@ -0,0 +1,13 @@
+package timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class App {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class);
+	}
+}
